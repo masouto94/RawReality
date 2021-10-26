02068985d408cf8d506bd5614ffbc88546437e8c
eliasjz36 <bseliasjimenez14@gmail.com>
2021-10-25 21:02:29 -0300

    debugging

--- rawreality/src/Components/Cards/ContactCard.jsx
+++ rawreality/src/Components/Cards/ContactCard.jsx
@@ -1,94 +1,99 @@
 import React from "react";
 import Card from "./Card";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import {faWhatsapp, faBehance, faLinkedin, faInstagram} from '@fortawesome/free-brands-svg-icons';
+import './ContactCard.css';
 
 const ContactCard = () => {
+    const iconStyles = {
+        color: "#BE9D94",
+        fontSize: "25px",
+    };
     return (
         <>
-            <Card display={"flex"} margin={"5% 20%"}>
+            <Card display={"flex"} margin={"12%"}> 
                 {/* INICIO CONTENIDO DEL LADO IZQUIERDO */}
-
-                <div id="ContactForm" className="text-start mx-5">
-                    <div className="titulo mb-5">
-                        <h2>¿Listo para contactarnos?</h2>
-                    </div>
-                    <div>
-                        <p>
-                            <i></i>
+                <div className="get-started text-start" style={{flex:"50%"}}>
+                    <h2 className="fs-1 mb-5 fw-bold">¿Listo para contactarnos?</h2>
+                    <ul className="list-unstyled">
+                        <li className="fw-bold text-start mb-4 d-flex">
+                            <FontAwesomeIcon className="me-2" style={iconStyles} icon={faEnvelope}/>
                             romgromero@gmail.com
-                        </p>
-                        <p>
-                            <i></i>
-                            <p>+54 9 11 51593906</p>
-                        </p>
-                        <p>
-                            <i></i>
+                        </li>
+                        <li className="fw-bold text-start mb-4 d-flex">
+                            <FontAwesomeIcon className="me-2" style={iconStyles} icon={faWhatsapp}/>
+                            +54 9 11 51593906
+                        </li>
+                        <li className="fw-bold text-start mb-4 d-flex">
+                            <FontAwesomeIcon className="me-2" style={iconStyles} icon={faBehance}/>
                             <a href="https://www.behance.net/rominaromero3">
                                 https://www.behance.net/rominaromero3
                             </a>
-                        </p>
-                        <p>
-                            <i></i>
+                        </li>
+                        <li className="fw-bold text-start mb-4 d-flex">
+                            <FontAwesomeIcon className="me-2" style={iconStyles} icon={faLinkedin}/>
                             <a href="https://www.linkedin.com/in/romina-romero-b64a7993/">
                                 https://www.linkedin.com/in/romina-romero-b64a7993/
                             </a>
-                        </p>
-                        <p>
-                            <i></i>
+                        </li>
+                        <li className="fw-bold text-start mb-4 d-flex">
+                            <FontAwesomeIcon className="me-2" style={iconStyles} icon={faInstagram}/>
                             <a href="https://www.instagram.com/romina_gabriela_romero_colombo/">
                                 https://www.instagram.com/romina_gabriela_romero_colombo/
                             </a>
-                        </p>
-                    </div>
+                        </li>
+                    </ul>
                 </div>
                 {/* FIN CONTENIDO DEL LADO IZQUIERDO */}
 
                 {/* INICIO CONTENIDO DEL LADO DERECHO */}
-                <div>
-                    <h3 className="mb-5">Déjanos tu mensaje.</h3>
+                <div className="form" style={{flex:"50%"}}>
+                    <h3 className="mb-5 fs-4 text-start">Déjanos tu mensaje.</h3>
 
-                    <form method="POST" id="form">
-                        <div className="m-2">
+                    <form method="POST" id="ContactForm">
+                        <div className="mb-3">
                             <input
                                 type="text"
                                 name="nombre"
-                                id="name"
+                                id="nombre"
                                 placeholder="Nombre"
-                                className="rounded w-100"
+                                className="w-100"
                                 required
                             />
                         </div>
-                        <div className="m-2">
+                        <div className="mb-3">
                             <input
                                 type="email"
                                 name="email"
                                 id="email"
                                 placeholder="Email"
-                                className="rounded w-100"
+                                className="w-100"
                                 required
                             />
                         </div>
-                        <div className="m-2">
+                        <div className="mb-3">
                             <input
                                 type="text"
                                 name="asunto"
                                 id="asunto"
                                 placeholder="Asunto"
-                                className="rounded w-100"
+                                className="w-100"
                                 required
                             />
                         </div>
-                        <div className="m-2">
+                        <div className="mb-3">
                             <textarea
                                 rows="5"
-                                name="message"
+                                name="mensaje"
                                 id="mensaje"
                                 placeholder="Mensaje"
-                                className="rounded bg-primary w-100"
+                                className="w-100"
                                 required
                             ></textarea>
                         </div>
                         <div>
-                            <button className="btn btn-primary rounded w-100" type="submit">Enviar</button>
+                            <button className="w-100 custom-button fw-bold" type="submit">Enviar</button>
                         </div>
                     </form>
                 </div>
