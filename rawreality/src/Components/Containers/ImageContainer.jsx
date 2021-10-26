33a6abc5ba0985968c2974eee2f256bae6e6698c
Matias Souto <msouto@keepcon.com>
2021-10-26 13:35:23 -0300

    Se corrige warn en whatsappbutton. Se agrega wrapper a imagecontainer

--- rawreality/src/Components/Containers/ImageContainer.jsx
+++ rawreality/src/Components/Containers/ImageContainer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const ImageContainer = ({image}) => {
     return (
         // <div style={{borderStyle:"solid",margin:"10%", borderWidth:"thin", borderColor:"black", borderRadius:"50%",alignItems:"center", verticalAlign:"middle" }}>
-        <div className={"ImageContainer"} style={{backgroundImage:`url(${image})`}}>
+        <div className={"ImageContainer"} >
+            <div className={"ImageWrapper"}>
+            <img src={image} alt="" />
+            </div>
         </div>
         // </div>
     )
